29c7a80683a76fb975f48ee61bc913f5eb3f0e14
Henricrazy <henriquegabriel289@gmail.com>
2024-06-06 20:57:00 -0300

    adicionei variaveis number, string. Fiz uma estrutura de descisão.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let.console ("ola mundo");
+
+
+let nome = "hercules";
+let niveis = ["prata", "ouro"];
+let xp = 5001;
+let x = 0
+
+// para saber em qual nivel o heroi esta basta mudar as expressoes da esquerda "<" para direita ">"
+if (x < xp) { 
+
+  console.log("O Herói de nome " + nome +  " está no nível de " + niveis[0]);
+
+}else  {
+    
+  console.log("O Herói de nome " + nome +  " está no nível de " + niveis[1]);
+}
+  
+
+
+
+
 
---
+++ newFile.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= xp; i++) {
+    if (i >= xp) {
+        console.log(nickName + " esta na patante Ouro!");
+    }
+    console.log(nickName + " esta na patante Ouro!");
+
+
+}
